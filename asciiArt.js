908941e7dfabd68b2a589a0112f67073eafb18db
Nolan Rousselle <rousseln@oregonstate.edu>
2026-04-13 18:16:38 -0700

    fourth commit with asciiArt updates

--- asciiArt.js
+++ asciiArt.js
@@ -2,4 +2,14 @@
 const inputString = process.argv[2];
 
 // Print
-console.log(inputString);
+//console.log(inputString);
+
+//new code to print the ascii art
+import figlet from "figlet";
+
+async function doStuff() {
+  const text = await figlet.text(inputString);
+  console.log(text);
+}
+
+doStuff();
